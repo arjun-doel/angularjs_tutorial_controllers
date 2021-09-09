5c897d43b5e64d50286b0602c500e0449c9bc836
Arjun Doel <arjundoel@gmail.com>
2021-09-09 15:52:17 +0100

    adding promist to get request:

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 var MainController = function($scope, $http) {
 
-  
+  var onUserComplete = function(response){
+    $scope.user = response.data;
+    console.log($scope.user)
+  }
+
+  var onError = function(response){
+    $scope.error = "oops could not fetch data!"
+  }
+
+  $http.get('https://api.github.com/users/robconery')
+    .then(onUserComplete, onError)
 
   $scope.message = "Hello Angular";
-  $scope.person = person;
 }
