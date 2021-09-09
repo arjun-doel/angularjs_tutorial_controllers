7779575d4daffd1c11cd57ab98bf6c9b5e051953
Arjun Doel <arjundoel@gmail.com>
2021-09-09 15:24:08 +0100

    using scope for obj

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
 var MainController = function($scope) {
+
+
+  var person = {
+    firstName: "Arjun",
+    lastName: "Doel",
+    age: 24,
+  }
+
+
   $scope.message = "Hello Angular";
+  $scope.person = person;
 }
